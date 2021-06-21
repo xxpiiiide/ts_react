385ff5095358b3405dfa34d9802c7323a5760223
이 필 제 <xxpiiiide@gmail.com>
2021-06-21 15:17:22 +0900

    useState Generics 설정

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 
 function Counter() {
     const [count, setCount] = useState<number>(0)   //useState를 사용할 때 Generics를 사용하여 해당 상태가 어떤 타입을 가지고 있을 지 설정해주면 된다.
+                                                    //사실상 useState<타입>을 설정해주지 않아도 무방하다. 
+                                                    //상태값이 null일수도 있고, 아닐수도 있을 경우 Generics를 활용하면 좋다. 
     const Increase = () => setCount(count +1)
     const Decrease = () => setCount(count -1)
     return(
