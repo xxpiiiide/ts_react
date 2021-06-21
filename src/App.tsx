0ebf8faa17463f8d8c1a6f6cf0ddc66d136b6bab
이 필 제 <xxpiiiide@gmail.com>
2021-06-21 21:04:00 +0900

    useReducer 샘플예시

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import MyForm from './MyForm'
+// import MyForm from './MyForm'
+// import Counter from './Counter'
+import ReducerSample from './ReducerSample'
 
 const App: React.FC = () => {
-  const onSubmit = (form: {name: string; description: string}) => {
-    console.log(form);
-  }
-  return <MyForm onSubmit={onSubmit} />
+  // const onSubmit = (form: {name: string; description: string}) => {
+  //   console.log(form);
+  // }
+  return <ReducerSample />
+  // <MyForm onSubmit={onSubmit} />
+  
 }
 
 export default App
