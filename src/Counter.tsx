21b3da392d9b7e08647849def6eb3d8733d33652
이 필 제 <xxpiiiide@gmail.com>
2021-06-21 17:08:27 +0900

    useReducer 사용

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,21 +1,32 @@
-import React, {useState} from 'react'
+import React, {useReducer} from 'react'
 
-function Counter() {
-    const [count, setCount] = useState<number>(0)   //useState를 사용할 때 Generics를 사용하여 해당 상태가 어떤 타입을 가지고 있을 지 설정해주면 된다.
-                                                    //사실상 useState<타입>을 설정해주지 않아도 무방하다. 
-                                                    //상태값이 null일수도 있고, 아닐수도 있을 경우 Generics를 활용하면 좋다. 
-    const Increase = () => setCount(count +1)
-    const Decrease = () => setCount(count -1)
+type Action = {type: 'INCREASE'} | {type: "DECREASE"}; // 보이듯 action을 | 을 이용하여 연달아 사용하기
+
+function reducer(state:number, action: Action): number {
+    switch(action.type) {
+        case 'INCREASE': 
+            return state +1;
+        case 'DECREASE':
+            return state -1;
+        default:
+            throw new Error('Unhandled action')
+    }
+}
+
+function Counter () {
+    const [count, dispatch] = useReducer(reducer, 0);
+    const onIncrease = () => dispatch({type: 'INCREASE'});
+    const onDecrease = () => dispatch({type: 'DECREASE'});
+
     return(
         <div>
             <h1>{count}</h1>
             <div>
-                <button onClick={Increase}>+1</button>
-                <button onClick={Decrease}>+1</button>
+                <button onClick={onIncrease}>+1</button>
+                <button onClick={onDecrease}>-1</button>
             </div>
-        </div>
+        </div>    
     )
-
 }
 
 
